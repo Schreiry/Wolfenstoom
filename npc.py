faab42de57e3b0baaa23c7c668b03ef5ac76038d
Schreiry <108213850+Schreiry@users.noreply.github.com>
2025-01-08 20:31:23 +0400

    Wolfenstoom
    Corrected errors, which have accumulated not a small number. I tried to expand the map. Support for more textures. Higher quality textures, small optimization, game settings, code changes . . . . . 
    further, only more!

--- npc.py
+++ npc.py
@@ -4,7 +4,7 @@ from random import randint, random
 
 class NPC(AnimatedSprite):
     def __init__(self, game, path='resources/sprites/npc/soldier/0.png', pos=(10.5, 5.5),
-                 scale=0.6, shift=0.38, animation_time=180):
+                 scale=0.9, shift=0.38, animation_time=180):
         super().__init__(game, path, pos, scale, shift, animation_time)
         self.attack_images = self.get_images(self.path + '/attack')
         self.death_images = self.get_images(self.path + '/death')
